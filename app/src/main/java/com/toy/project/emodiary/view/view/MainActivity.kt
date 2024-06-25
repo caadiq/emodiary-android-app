26406ec5b3b5ef010d243ab418cef1aa5ffd3c74
caadiq <caadiq@gmail.com>
2024-06-25 13:54:16 +0900

    bottom navigation 프래그먼트 전환

--- app/src/main/java/com/toy/project/emodiary/view/view/MainActivity.kt
+++ app/src/main/java/com/toy/project/emodiary/view/view/MainActivity.kt
@@ -1,14 +1,83 @@
 package com.toy.project.emodiary.view.view
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.toy.project.emodiary.R
 import com.toy.project.emodiary.databinding.ActivityMainBinding
+import com.toy.project.emodiary.view.viewmodel.MainFragmentType
+import com.toy.project.emodiary.view.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    private var backPressedTime: Long = 0
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime >= 2000) {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(this@MainActivity, "뒤로 가기 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        if (savedInstanceState == null) {
+            setupFragment()
+        }
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            add(binding.fragmentContainerView.id, HomeFragment(), MainFragmentType.HOME.tag)
+            add(binding.fragmentContainerView.id, ProfileFragment(), MainFragmentType.PROFILE.tag)
+            commit()
+        }
+    }
+
+    private fun setupView() {
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> mainViewModel.setCurrentFragment(0)
+                R.id.profile -> mainViewModel.setCurrentFragment(1)
+            }
+            true
+        }
+    }
+
+    private fun setupViewModel() {
+        mainViewModel.currentFragmentType.observe(this) { fragmentType ->
+            val currentFragment = supportFragmentManager.findFragmentByTag(fragmentType.tag)
+            supportFragmentManager.beginTransaction().apply {
+                setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                supportFragmentManager.fragments.forEach { fragment ->
+                    if (fragment == currentFragment)
+                        show(fragment)
+                    else
+                        hide(fragment)
+                }
+            }.commit()
+
+            binding.bottomNav.selectedItemId = when (fragmentType) {
+                MainFragmentType.HOME -> R.id.home
+                MainFragmentType.PROFILE -> R.id.profile
+                else -> R.id.home
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/toy/project/emodiary/view/viewmodel/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.toy.project.emodiary.view.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+enum class MainFragmentType(val tag: String) {
+    HOME("home"),
+    PROFILE("profile")
+}
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _currentFragmentType = MutableLiveData<MainFragmentType>(MainFragmentType.HOME)
+    val currentFragmentType: LiveData<MainFragmentType> = _currentFragmentType
+
+    fun setCurrentFragment(item: Int): Boolean {
+        val pageType = getPageType(item)
+        changeCurrentFragmentType(pageType)
+
+        return true
+    }
+
+    private fun getPageType(item: Int): MainFragmentType {
+        return when (item) {
+            0 -> MainFragmentType.HOME
+            1 -> MainFragmentType.PROFILE
+            else -> MainFragmentType.HOME
+        }
+    }
+
+    private fun changeCurrentFragmentType(fragmentType: MainFragmentType) {
+        if (currentFragmentType.value == fragmentType)
+            return
+
+        _currentFragmentType.value = fragmentType
+    }
+}
